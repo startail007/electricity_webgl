c3fbf27875f4bd99d1dafe494d6160282b373420
cai <flashmx29403229@gmail.com>
2020-11-26 23:55:00 +0800

    更新

--- src/shader/electricity.fs
+++ src/shader/electricity.fs
@@ -1,6 +1,6 @@
 precision mediump float;
 varying vec2 vTextureCoord;
-uniform lowp vec2 uSize;
+uniform vec2 uSize;
 uniform float uTime;
 
 uniform float uLineWidth;
@@ -135,34 +135,23 @@ vec4 distLightLine(vec2 p,vec2 a,vec2 b,float w){
 vec2 swingFun(vec2 uv,vec2 startPos,vec2 endPos,float power,float offset,float time){
   vec2 v=endPos-startPos;
   v=normalize(v);
-  vec2 uv0=uv*clamp(500./max(power,10.),1.75,10.);
+  vec2 uv0=uv*clamp(1./max(power,.01),.01,5.);
   float a=0.;
-  if(power>100.){
+  if(power>.01){
     a=fbm_noise_perlin(vec3(uv0,time));
   }else{
     a=abs(noise_perlin(vec3(uv0,time*2.)))-.5;
   }
   v=vec2(-v.y,v.x);
   vec2 move=a*vec2(cos(a*2.*pi),sin(a*2.*pi))+offset*v;
-  return move*.3*power/length(uSize);
+  return move*.3*power/length(1.5);
   
 }
 vec4 elBall(vec2 uv,vec2 pos,float r,float time){
-  /*vec2 v=uv-pos;
-  float len00=length(v);
-  float a00=.5*atan(v.x,v.y)/pi+.5;
-  float a=noise_perlin(vec2(a00*10.,len00*.2+time),vec2(10.,100.));
-  vec2 move=30.*a*normalize(vec2(cos(a*2.*pi),sin(a*2.*pi))+v*.2);
-  return.5*drawLight(length(v+move)/r);*/
-  
-  /*float a=fbm_abs_noise_perlin(vec3(uv*0.05,time));
-  vec2 move=10.*a*vec2(cos(a*2.*pi),sin(a*2.*pi));
-  float len = length(uv-pos+move);
-  return drawLight(len)+0.5*drawLight(a*len/min(r*4.,1.))*clamp(1.-len/200.,0.,1.);*/
-  float len = length(uv-pos);
-  if(len<25.){
-    float a=abs(noise_perlin(vec3(uv*0.1,time)));
-    return 0.15*drawLight(a*len/min(r*4.,1.))*pow(clamp(1.-len/25.,0.,1.),3.);
+  float len=length(uv-pos);
+  if(len<.05){
+    float a=abs(noise_perlin(vec3(uv*200.,time)));
+    return.15*drawLight(a*len*400.)*pow(clamp(1.-len/.05,0.,1.),3.);
   }else{
     return vec4(0.);
   }
@@ -171,23 +160,28 @@ vec4 elBall(vec2 uv,vec2 pos,float r,float time){
 void main()
 {
   vec4 color=vec4(0.);
-  vec2 coord=vTextureCoord*uSize;
-  float d=distLine(coord,uStartPos,uEndPos,1.);
-  float len=distance(uStartPos,uEndPos);
-  float d1=distLine(coord,uBranchStartPos,uBranchEndPos,1.);
-  float len1=distance(uBranchStartPos,uBranchEndPos);
   vec2 aspectRatio=vec2(uSize.x/uSize.y,1.);
-  if(d<max(len,200.)||(uBranchBool&&d1<max(len1,200.))){
+  vec2 coord=vTextureCoord*aspectRatio;
+  vec2 startPos=uStartPos/uSize.y;
+  vec2 endPos=uEndPos/uSize.y;
+  vec2 branchStartPos=uBranchStartPos/uSize.y;
+  vec2 branchEndPos=uBranchEndPos/uSize.y;
+  float lineWidth=uLineWidth/uSize.y;
+  //float d=distLine(coord,startPos,endPos,1.);
+  float len=distance(startPos,endPos);
+  //float d1=distLine(coord,branchStartPos,branchEndPos,1.);
+  float len1=distance(branchStartPos,branchEndPos);
+  //if(d<max(len,200.)||(uBranchBool&&d1<max(len1,200.))){
     
     {
-      float gradualVal=gradual(coord,uStartPos,uEndPos);
+      float gradualVal=gradual(coord,startPos,endPos);
       float swing=sin(gradualVal*pi);
       swing=1.-pow(1.-swing,2.);
       
       float gradualVal1=0.;
       float branchSwing=0.;
       if(uBranchBool){
-        gradualVal1=gradual(coord,uBranchStartPos,uBranchEndPos);
+        gradualVal1=gradual(coord,branchStartPos,branchEndPos);
         branchSwing=sin(gradualVal1*pi);
         branchSwing=1.-pow(1.-branchSwing,2.);
         if(!uStartFixed&&uEndFixed){
@@ -201,64 +195,64 @@ void main()
         }
       }
       
-      vec2 uv001=vTextureCoord;
+      vec2 uv001=vTextureCoord*aspectRatio;
       if(swing!=0.){
-        uv001+=swing*swingFun(1.2*uv001*aspectRatio,uStartPos,uEndPos,1.2*len,uOffset,uTime*3.);
+        uv001+=swing*swingFun(1.2*uv001*aspectRatio,startPos,endPos,1.2*len,uOffset,uTime*3.);
         if(uBranchBool){
           if(branchSwing!=0.){
-            uv001+=gradualVal1*branchSwing*swingFun(1.2*uv001*aspectRatio,uBranchStartPos,uBranchEndPos,1.2*len1,uBranchOffset,uTime*3.);
+            uv001+=gradualVal1*branchSwing*swingFun(1.2*uv001*aspectRatio,branchStartPos,branchEndPos,1.2*len1,uBranchOffset,uTime*3.);
           }
         }
       }
       
-      float r=clamp(pow(.5*length(uSize)/len,2.),uLineWidth,uLineWidth*2.);
+      float r=clamp(pow(.5*length(1.5)/len,2.),lineWidth,lineWidth*2.);
       //uv001*=uSize;
       float gradualVal2=gradualVal;
       if(uBranchBool){
         gradualVal2=gradualVal1;
       }
-      r*=mix(1.-.5*sin(gradualVal2*pi),3.,clamp(10./len,0.,1.));
+      r*=mix(1.-.5*sin(gradualVal2*pi),3.,clamp(.01/len,0.,1.));
       
       if(uBranchBool){
-        color+=distLightLine(uv001,uBranchStartPos/uSize,uBranchEndPos/uSize,r/uSize.y);
+        color+=distLightLine(uv001,branchStartPos,branchEndPos,r);
       }else{
-        color+=distLightLine(uv001,uStartPos/uSize,uEndPos/uSize,r/uSize.y);
+        color+=distLightLine(uv001,startPos,endPos,r);
       }
       
-      /*if(uBranchBool){
+      if(uBranchBool){
         if(uStartFixed){
-          color+=elBall(coord,uBranchStartPos,1./len1,uTime*3.);
+          color+=elBall(coord,branchStartPos,1./len1,uTime*3.);
         }
         if(uEndFixed){
-          color+=elBall(coord,uBranchEndPos,1./len1,uTime*3.);
+          color+=elBall(coord,branchEndPos,1./len1,uTime*3.);
         }
       }else{
         if(uStartFixed){
-          color+=elBall(coord,uStartPos,1./len,uTime*3.);
+          color+=elBall(coord,startPos,1./len,uTime*3.);
         }
         if(uEndFixed){
-          color+=elBall(coord,uEndPos,1./len,uTime*3.);
+          color+=elBall(coord,endPos,1./len,uTime*3.);
         }
-      }*/
-    }
+      }
+    //}
     
     /*if(uBranchBool){
       if(uStartFixed){
-        color+=elBall(coord,uBranchStartPos,len1*.02,-uTime*3.);
+        color+=elBall(coord,branchStartPos,len1*.02,-uTime*3.);
       }
       if(uEndFixed){
-        color+=elBall(coord,uBranchEndPos,len1*.02,uTime*3.);
+        color+=elBall(coord,branchEndPos,len1*.02,uTime*3.);
       }
     }else{
       if(uStartFixed){
-        color+=elBall(coord,uStartPos,len*.02,-uTime*3.);
+        color+=elBall(coord,startPos,len*.02,-uTime*3.);
       }
       if(uEndFixed){
-        color+=elBall(coord,uEndPos,len*.02,uTime*3.);
+        color+=elBall(coord,endPos,len*.02,uTime*3.);
       }
     }*/
     /*{
-      vec2 uv=coord-uEndPos;
+      vec2 uv=coord-endPos;
       float len=length(uv);
       float noise0=abs(noise_perlin(vec2((20.*(.5*atan(uv.y,uv.x)/pi+.5)),len/20.-uTime*5.),vec2(20.,0.)));
       float noise1=abs(noise_perlin(vec2((20.*(.5*atan(uv.y,uv.x)/pi+.5)),len/20.-uTime*5.+1.),vec2(20.,0.)));
